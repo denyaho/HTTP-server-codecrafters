5750fcdab6e923ff8e692de30ecff90ca7b90568
小川大地 <ogawadaichi@ogawadaichinoMacBook-Air.local>
2026-04-18 20:17:29 +0900

    modified get.go file

--- internal/handler/get.go
+++ internal/handler/get.go
@@ -1,15 +1,11 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/codecrafters-io/http-server-starter-go/internal/parser"
 	"github.com/codecrafters-io/http-server-starter-go/internal/config"
 	"net"
 	"strings"
-	"os"
-	"path/filepath"
-	"bytes"
-	"compress/gzip"
+	"github.com/codecrafters-io/http-server-starter-go/internal/response"
 )
 
 var SupportedEncoding = map[string]bool{
@@ -18,49 +14,20 @@ var SupportedEncoding = map[string]bool{
 }
 
 func HandleGet(request *parser.Request, conn net.Conn, config *config.Config) {
-	if request.Path == "/" {
-		conn.Write([]byte(fmt.Sprintf("%s 200 OK\r\n\r\n", request.Version)))
-	}else if strings.HasPrefix(request.Path, "/echo/") {
-		words := request.Path[len("/echo/"):]
-		encoding, ok := request.Headers["Accept-Encoding"]
-		if ok{
-			encodings := strings.Split(encoding, ",")
-			encode := "invalid-encoding"
-			for _, e := range encodings {
-				if SupportedEncoding[strings.TrimSpace(e)]{
-					encode = strings.TrimSpace(e)
-					break
-				}
-			}
-			switch encode {
-				case "gzip":
-					var buf bytes.Buffer
-					zw := gzip.NewWriter(&buf)
-					if _, err := zw.Write([]byte(words)); err != nil {
-						conn.Write([]byte(fmt.Sprintf("%s 500 Internal Server Error\r\n\r\n", request.Version)))
-					}
-					zw.Close()
-					compressed := buf.Bytes()
-					conn.Write([]byte(fmt.Sprintf("%s %d OK\r\nContent-Type: %s\r\nContent-Encoding: %s\r\nContent-Length: %d\r\n\r\n%s", request.Version, 200, "text/plain", "gzip", len(compressed), compressed)))
-				default:
-					conn.Write([]byte(fmt.Sprintf("%s %d OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", request.Version, 200, "text/plain", len(words), words)))
-			}
-		}else{
-			conn.Write([]byte(fmt.Sprintf("%s %d OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", request.Version, 200, "text/plain", len(words), words)))
-		}
-
-	}else if request.Path == "/user-agent" {
-		user_agent := request.Headers["User-Agent"]
-		conn.Write([]byte(fmt.Sprintf("%s %d OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", request.Version, 200, "text/plain", len(user_agent), user_agent)))
-	} else if strings.HasPrefix(request.Path, "/files"){
-		file_path := request.Path[len("/files/"):]
-		data, err := os.ReadFile(filepath.Join(config.DirName, file_path))
-		if err != nil {
-			conn.Write([]byte(fmt.Sprintf("%s 404 Not Found\r\n\r\n", request.Version)))
-			return
-		}
-		conn.Write([]byte(fmt.Sprintf("%s %d OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", request.Version, 200, "application/octet-stream", len(data), data)))
-	}else{
-		conn.Write([]byte(fmt.Sprintf("%s 404 Not Found\r\n\r\n", request.Version)))
+	res := &response.Response{
+		Headers: make(map[string]string),
 	}
+	if request.Path == "/" {
+		response.HandleRoot(res)
+	}else if strings.HasPrefix(request.Path, "/echo/") {
+		response.HandleEcho(res, request)
+	}else if request.Path == "/user-agent" {
+		response.HandleUserAgent(res, request)
+	} else if strings.HasPrefix(request.Path, "/files"){
+		response.HandleFiles(res, request, config)
+	}else{
+		response.HandleNotFound(res)
+	}
+	keepalive := request.Headers["Connection"] != "close"
+	res.Write(conn, keepalive)
 }
---
+++ internal/response/response.go
@@ -0,0 +1,138 @@
+package response
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"net"
+
+	"github.com/codecrafters-io/http-server-starter-go/internal/parser"
+	"github.com/codecrafters-io/http-server-starter-go/internal/config"
+	"strings"
+	"os"
+)
+
+var SupportedEncodings = map[string]bool{
+	"gzip": true,
+	"deflate": true,
+}
+
+type Response struct {
+	Version string
+	StatusCode int
+	StatusText string
+	Headers map[string]string
+	Body []byte
+}
+
+
+func HandleRoot(response *Response) {
+	response.Version = "HTTP/1.1"
+	response.StatusCode = 200
+	response.StatusText = "OK"
+	response.Headers["Content-Type"] = "text/plain"
+}
+
+func HandleEcho(response *Response, request *parser.Request) {
+	response.Version = "HTTP/1.1"
+	words := request.Path[len("/echo/"):]
+	encoding, ok := request.Headers["Accept-Encoding"]
+	if ok {
+		encodings := strings.Split(encoding, ",")
+		encode := "invalid-encoding"
+		for _, e := range encodings {
+			if SupportedEncodings[strings.TrimSpace(e)] {
+				encode = strings.TrimSpace(e)
+				break
+			}
+		}
+		switch encode {
+			case "gzip":
+				var buf bytes.Buffer
+				zw := gzip.NewWriter(&buf)
+				if _, err := zw.Write([]byte(words)); err != nil {
+					response.StatusCode = 500
+					response.StatusText = "Internal Server Error"
+					return
+				}
+				zw.Close()
+				compressed := buf.Bytes()
+				response.StatusCode = 200
+				response.StatusText = "OK"
+				response.Headers["Content-Type"] = "Text/plain"
+				response.Headers["Content-Encoding"] = "gzip"
+				response.Headers["Content-Length"] = fmt.Sprintf("%d", len(compressed))
+				response.Body = compressed
+			default:
+				response.StatusCode = 200
+				response.StatusText = "OK"
+				response.Headers["Content-Type"] = "text/plain"
+				response.Headers["Content-Length"] = fmt.Sprintf("%d", len(words))
+				response.Body = []byte(words)
+		}
+	}else{
+		response.StatusCode = 200
+		response.StatusText = "OK"
+		response.Headers["Content-Type"] = "text/plain"
+		response.Headers["Content-Length"] = fmt.Sprintf("%d", len(words))
+		response.Body = []byte(words)
+	}
+}
+
+func HandleUserAgent(response *Response, request *parser.Request) {
+	response.Version = "HTTP/1.1"
+	response.StatusCode = 200
+	response.StatusText = "OK"
+	response.Headers["Content-Type"] = "text/plain"
+	response.Headers["User-Agent"] = request.Headers["User-Agent"]
+	response.Headers["Content-Length"] = fmt.Sprintf("%d", len(request.Headers["User-Agent"]))
+	response.Body = []byte(request.Headers["User-Agent"])
+}
+
+func HandleFiles(response *Response, request *parser.Request, config *config.Config) {
+	response.Version = "HTTP/1.1"
+	file_name := request.Path[len("/files/"):]
+	full_path := config.DirName + "/" + file_name
+	data, err := os.ReadFile(full_path)
+	if err != nil {
+		response.StatusCode = 404
+		response.StatusText = "Not Found"
+		return
+	}
+	response.StatusCode = 200
+	response.StatusText = "OK"
+	response.Headers["Content-Type"] = "application/octet-stream"
+	response.Headers["Content-Length"] = fmt.Sprintf("%d", len(data))
+	response.Body = data
+}
+
+func HandleNotFound(response *Response) {
+	response.Version = "HTTP/1.1"
+	response.StatusCode = 404
+	response.StatusText = "Not Found"
+}
+
+func (r *Response) Write(conn net.Conn, keepAlive bool) {
+	if keepAlive {
+		r.Headers["Connection"] = "keep-alive"
+	}else {
+		r.Headers["Connection"] = "close"
+	}
+	status_line := fmt.Sprintf("%s %d %s\r\n", r.Version, r.StatusCode, r.StatusText)
+	header_lines := fmt.Sprintf("Content-Type: %s\r\nContent-Length: %d	\r\nConnection: %s\r\n", r.Headers["Content-Type"], len(r.Body), r.Headers["Connection"])
+	switch r.StatusCode {
+		case 200:
+			if r.Body == nil && r.Headers == nil {
+				conn.Write([]byte(status_line + "\r\n"))
+				return
+			} else if r.Headers["Content-Encoding"] != "" {
+				header_lines = header_lines + fmt.Sprintf("Content-Encoding: %s\r\n", r.Headers["Content-Encoding"])
+			}
+			body_line := fmt.Sprintf("\r\n%s", r.Body)
+			conn.Write([]byte(status_line + header_lines + body_line))
+		case 404:
+			conn.Write([]byte(status_line + "\r\n"))
+		case 500:
+			conn.Write([]byte(status_line + "\r\n"))
+	}
+}
